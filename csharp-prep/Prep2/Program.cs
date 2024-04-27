01dcecc63760063a5200bfcc2d126a807ad37cf6
j4yu22 <jay.e.underwood@gmail.com>
2024-04-27 12:36:59 -0600

    and this too?

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    Console.WriteLine("Enter your grade percentage as an integer: ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 60)
+        {
+            int lastDigit = grade % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        else if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You pass :)");
+        }
+        else
+        {
+            Console.WriteLine("You fail :(");
+        }
     }
 }
