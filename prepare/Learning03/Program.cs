eca984db5d0e9c5ad1153b8a82832014f23dfa8d
jayunderwood <jay.e.underwood@gmail.com>
2024-05-22 11:45:19 -0600

    finished prepare activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(3, 4);
+        Fraction fraction4 = new Fraction(1, 3);
+
+        DisplayFractionInfo(fraction1);
+        DisplayFractionInfo(fraction2);
+        DisplayFractionInfo(fraction3);
+        DisplayFractionInfo(fraction4);
+
+        fraction1.SetNumerator(5);
+        fraction1.SetDenominator(8);
+        DisplayFractionInfo(fraction1);
+    }
+
+    static void DisplayFractionInfo(Fraction fraction)
+    {
+        Console.WriteLine($"{fraction.GetFractionString()} = {fraction.GetDecimalValue()}");
     }
 }
