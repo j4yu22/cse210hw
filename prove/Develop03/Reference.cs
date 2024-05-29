3d622c37474494572d885b515de76499e827c301
jayunderwood <jay.e.underwood@gmail.com>
2024-05-29 16:17:35 -0600

    took notes

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,2 @@
+//reference can take the words list as empty because each instance of reference needs the list<words>
+//look into an override here too for looping through printing the words
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,1 +1,2 @@
 // look into overwriting the 2 string method for hiding words and printing them.
+//override lets you override an inheritied method
